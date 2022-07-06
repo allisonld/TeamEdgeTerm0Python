5991fc66ed7fbb3aca47566aedf5d2cee5747190
allisonld <allison.dana@csedge.org>
2022-07-06 14:43:36 -0700

    day1TeamEdgeCompletedChallenges--firstcommit first commit

--- variables.py
+++ variables.py
@@ -4,6 +4,8 @@
 # -------------------------------------------- 
 
 # Example
+
+
 message = "Hello World!"
 print(message)
 
@@ -15,8 +17,10 @@ print(message)
 # -------------------------------------------- 
 
 print("------------------- Challenge 1 -------------------")
-
-print(f"Hi! My name is {name}")
+name = "Allison"
+grade = 12
+funFact = "I wrote a novel in 9th grade"
+print(f"Hi! My name is {name}.")
 print(f"I'm in the {grade}th grade.")
 print(f"A fun fact about me is that {funFact}")
 
@@ -29,6 +33,10 @@ print(f"A fun fact about me is that {funFact}")
 # **** Upchallenge! ****
 # Can you also print out the data type of the variables? 
 
+print(type(name))
+print(type(grade))
+print(type(funFact))
+
 
 
 
@@ -36,14 +44,18 @@ print(f"A fun fact about me is that {funFact}")
 # Create a string for the different lines of the Happy Birthday Song
 # Print out the song using the strings you declared.
 
+firstLine = "happy birthday to you"
+secondLine = firstLine
+thirdLine = "happy birthday dear faith"
+fourthLine = firstLine
 
-
+print(f"{firstLine} {secondLine} {thirdLine} {fourthLine}")
 
 # **** Challenge 1: Problem 3 ****
 # Print out a string that has brackets in it.
 
 
-
+print("here are some numbers[4]")
 
 # -------------------------------------------- 
 # Challenge 2: 
@@ -60,38 +72,48 @@ num2 = 10
 # **** Challenge 2: Problem 1 ****
 # Store the sum of num1 and num2 in a variable.
 # Print the sum.
+sum = num1 + num2
+print(sum)
+
+# # **** Challenge 2: Problem 2 ****
+# # Store the difference between sum (above) and 7 in a variable.
+# # Print the difference.
+differencebtwn7andSum = 7 - sum
+
+print(differencebtwn7andSum)
+
+# # **** Challenge 2: Problem 3 ****
+# # Store the product of the difference and 3 in a variable.
+# # Print the product.
+
+productOfDifferenceAnd3 = differencebtwn7andSum * 3
+print(productOfDifferenceAnd3)
+
+# # **** Challenge 2: Problem 4 ****
+# # Store the power of the product squared in a variable.
+# # Print the power.
+
+powerofProductSquared = productOfDifferenceAnd3 * productOfDifferenceAnd3
+print(powerofProductSquared)
+# # **** Challenge 2: Problem 5 ****
+# # Store the quotient of the power divided by 4 in a variable.
+# # Print the quotient.
+
+quotientOfThePowerDividedBy4 = powerofProductSquared / 4
+
+# # **** Challenge 2: Problem 6 ****
+# # Store the remainder of the quotient divided by 2 in a variable.
+# # Print the remainder.
+
+remainderquotientDividedBy2 = quotientOfThePowerDividedBy4 % 2
+print(remainderquotientDividedBy2)
+
+# # **** Upchallenge! ****
+# # Given a variable that stores a number, print the values that come right before and right after.
+# # For example, given the number 4, the program should print 3 and 5. 
+# # Your code should work if the value of the variable is changed.
+
+# num = 9
 
 
-# **** Challenge 2: Problem 2 ****
-# Store the difference between sum (above) and 7 in a variable.
-# Print the difference.
-
-
-# **** Challenge 2: Problem 3 ****
-# Store the product of the difference and 3 in a variable.
-# Print the product.
-
-
-# **** Challenge 2: Problem 4 ****
-# Store the power of the product squared in a variable.
-# Print the power.
-
-
-# **** Challenge 2: Problem 5 ****
-# Store the quotient of the power divided by 4 in a variable.
-# Print the quotient.
-
-
-# **** Challenge 2: Problem 6 ****
-# Store the remainder of the quotient divided by 2 in a variable.
-# Print the remainder.
-
-
-# **** Upchallenge! ****
-# Given a variable that stores a number, print the values that come right before and right after.
-# For example, given the number 4, the program should print 3 and 5. 
-# Your code should work if the value of the variable is changed.
-
-num = 9
-
-# -------------------------------------------- 
+# # -------------------------------------------- 
